0390561bc373192bacf86d023cdd829bcb29a7fd
Kurt Zikaras <KurtZikaras@Kurts-MacBook-Pro.local>
2017-09-12 17:38:26 -0400

    complete checkpoint-14

---
+++ db/migrate/20170912005706_create_comments.rb
@@ -0,0 +1,11 @@
+class CreateComments < ActiveRecord::Migration[5.1]
+  def change
+    create_table :comments do |t|
+      t.text :body
+      # #3
+      t.references :post, index: true, foreign_key: true
+
+      t.timestamps null: false
+    end
+  end
+end
