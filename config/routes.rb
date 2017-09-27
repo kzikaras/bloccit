b0122179e1c086cefa28fefd087cc4372ccba03f
Kurt Zikaras <KurtZikaras@Kurts-MacBook-Pro.local>
2017-09-26 22:23:29 -0400

    save progress

--- config/routes.rb
+++ config/routes.rb
@@ -36,6 +36,8 @@ Rails.application.routes.draw do
 
   resources :users, only: [:new, :create]
 
+  resources :sessions, only: [:new, :create, :destroy]
+
   post 'users/confirm' => 'users#confirm'
 
   # #2
