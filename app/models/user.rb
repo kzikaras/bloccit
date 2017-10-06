a68c2c04b71ec2b4784a7b3d17ac44209118a68e
Kurt Zikaras <KurtZikaras@Kurts-MacBook-Pro.local>
2017-10-06 18:31:52 -0400

    complete checkpoint-29

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
     has_many :posts
+    has_many :comments
 
     def screen
         logger.info " @@@@@@@@@@@@@@@@@  FIELD ARE: #{self.name}"
--- config/routes.rb
+++ config/routes.rb
@@ -1,37 +1,16 @@
 Rails.application.routes.draw do
-  # get 'sponsored_posts/show'
-
-  # get 'sponsored_posts/new'
-
-  # get 'sponsored_posts/edit'
-
-  # get 'questions/index'
-
-  # get 'questions/new'
-
-  # get 'questions/create'
-
-  # get 'questions/show'
-
-  # get 'questions/edit'
-
-  # get 'questions/update'
-
-  # get 'questions/destroy'
-
-  # get 'advertisements/index'
-
-  # get 'advertisements/show'
-
-  # get 'advertisements/new'
-
-  # get 'advertisements/create'
 
   # #1
   resources :advertisements, :questions
   resources :topics do
     resources :posts, except: [:index]
     resources :sponsored_posts, except: [:index]
+  end
+
+  # #4
+  resources :posts, only: [] do
+    # #5
+    resources :comments, only: [:create, :destroy]
   end
 
   resources :users, only: [:new, :create]
