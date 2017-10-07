ce7771f426943b6d0e27ec5fbf3b84ef623aedfb
Kurt Zikaras <KurtZikaras@Kurts-MacBook-Pro.local>
2017-10-07 13:03:43 -0400

    complete checkpoint-30

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -64,6 +64,23 @@ RSpec.describe Post, type: :model do
       end
     end
 
+    describe "#create_vote" do
+      it "upvotes post to 1" do
+        expect(post.up_votes).to eq(1)
+      end
+
+      it "calls #create_vote when post is created" do
+        post = topic.posts.new(title: RandomData.random_sentence, body: RandomData.random_sentence, user: user)
+        expect(post).to receive(:create_vote)
+        post.save
+      end
+
+      it "associates vote with the user" do
+        expect(post.votes.first.user).to eq(post.user)
+      end
+
+    end
+
     describe "#update_rank" do
       # #28
       it "calculates the correct rank" do
@@ -83,6 +100,8 @@ RSpec.describe Post, type: :model do
         expect(post.rank).to eq (old_rank - 1)
       end
     end
+
+    
   end
 
 end
